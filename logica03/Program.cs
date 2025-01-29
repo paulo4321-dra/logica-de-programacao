0f5f6ac0b75438a3b1111133876790e0d3d115f9
PAULO FRANCA <paulo.franca@senacsp.edu.br>
2025-01-29 16:07:33 -0300

    logicadeprogamacao

---
+++ logica03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+
+            // Operadores relacionais
+
+            bool maiorQue = a > b;
+            bool menorQue = a < b;
+            bool igualA = a == b;
+            bool diferente = a != b;
+            bool maiorOuIgual = a >= b;
+            bool menorOuIgual = a <= b;
+
+            // Exibindo os resultados dos operadores relacionais
+
+            Console.WriteLine("\n Operadores relacionais :");
+            Console.WriteLine($"{a} > {b}: {maiorQue}");
+            Console.WriteLine($" {a} < {b} : {menorQue}");
+            Console.WriteLine($"{a} == {b} : {igualA}");
+            Console.WriteLine($" {a} != {b} : {diferente}");
+            Console.WriteLine($" {a} >= {b} : {maiorOuIgual}");
+            Console.WriteLine($" {a} <= {b} : {menorOuIgual}");
+
+            // Operadores Lógicos
+            bool x = true;
+            bool y = false;
+            bool and = x && y;  // E lógico
+            bool or = x || y;   // Ou lógico
+            bool not = !x;      // Não lógico
+
+            // Exibindo resultados dos operadores lógicos
+            Console.WriteLine("\nOperadores Lógicos:");
+            Console.WriteLine($"x && y: {and}");   // E lógico
+            Console.WriteLine($"x || y: {or}");    // Ou lógico
+            Console.WriteLine($"!x: {not}");       // Não lógico
+
+          Console.ReadLine();
+        }
+    }
+}
---
+++ logica05/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+         // While
+
+            int contador = 1;
+
+            while (contador <= 5)
+            {
+                Console.WriteLine(contador);
+                contador++;  // Icrmenta o contador
+            }
+
+            // Do while
+            int novoContador = 2;
+
+            do
+            {
+                Console.WriteLine(novoContador);
+                novoContador++; // Incrementa o contador
+
+            } while (novoContador <= 5);
+
+            // Laço Foreach
+            // Uma lista de strings
+            List<string> nomes = new List<string> {"Ana", "Carlos", "João", "Maria"};
+
+           // Usando o Foreach para percorrer a lista 
+           foreach ( string nome in nomes)
+            {
+                Console.WriteLine(nome); // Imprime cada nome
+            }
+
+            //Um vetor de números inteiros
+            int[] numeros = { 1, 2, 3, 4, 5 };
+
+            // Usando o foreach para percorrer o vetor
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero); // Imprime cada número
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
